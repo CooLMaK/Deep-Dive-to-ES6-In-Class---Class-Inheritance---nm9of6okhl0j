895bce645abc65522bb89fddcce7c3b8d837cc67
Mayank Ranjan <96287376+CooLMaK@users.noreply.github.com>
2022-02-23 23:28:23 +0530

    make changes

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape{
+    //let color;
+    constructor(c){
+        this.color = c;
+    }
+    drawShape(){
+        
+    }
+    calculateArea(){
 
+    }
+    
+}
 // export class using module.exports
+module.exports=Shape;
